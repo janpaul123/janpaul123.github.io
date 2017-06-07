989f8cb32fd4cec2d0c154eb53f09c949d3a4595
Jan Paul Posma <janpaul123@users.noreply.github.com>
2017-06-07 00:05:44 -0700

    Add craziness to “10 minutes” article

--- 10-minutes/index.js
+++ 10-minutes/index.js
@@ -29,19 +29,72 @@ function addVisibilityHandler(func) {
 
 
 $(function() {
+  // Wrap everything in single letter spans.
+  {
+    // From https://stackoverflow.com/a/10730777
+    function textNodesUnder(el) {
+      var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+      while(n=walk.nextNode()) a.push(n);
+      return a;
+    }
+    var allTextNodes = textNodesUnder(document.querySelector('article'));
+
+    allTextNodes.forEach(function(node) {
+      // https://stackoverflow.com/a/15553884
+      var replacementNode = document.createElement('span');
+      replacementNode.innerHTML = node.textContent.replace(/\w/g, function(text) {
+        return '<span class="single-letter">' + text + '</span>';
+      });
+      node.parentNode.insertBefore(replacementNode, node);
+      node.parentNode.removeChild(node);
+    });
+  }
+
+  var singleLetterSpans = Array.from(document.querySelectorAll('.single-letter'));
+  var fractionsWhenToIntroduceCraziness = _.shuffle(singleLetterSpans.map(function(__, index) {
+    return index / singleLetterSpans.length;
+  }));
+  var crazyCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz0123456789';
+  var originalTexts = singleLetterSpans.map(function(span) {
+    return span.textContent;
+  });
+
+  singleLetterSpans.forEach(function(span) {
+    span.style.width = span.offsetWidth + 'px';
+  });
+
+  var clockSecondsStartOfCraziness = 60 * 8;
   var clockSeconds = 60 * 10;
-  var intervalMs = 1000;
+  var decreaseMs = 800;
+  var intervalMs = 60;
+  var enabled = true;
 
   function tickClock() {
-    clockSeconds--;
+    if (!enabled) return;
+
+    clockSeconds -= intervalMs / decreaseMs;
     if (clockSeconds < 0) {
       clockSeconds = 0;
     }
-    var minutes = Math.floor(clockSeconds / 60);
+    var roundedClockSeconds = Math.round(clockSeconds);
+    var minutes = Math.floor(roundedClockSeconds / 60);
     if (minutes < 10) minutes = '0' + minutes;
-    var seconds = clockSeconds % 60;
+    var seconds = roundedClockSeconds % 60;
     if (seconds < 10) seconds = '0' + seconds;
     $('.counter-numbers').text('00:' + minutes + ':' + seconds);
+
+    fractionsWhenToIntroduceCraziness.forEach(function(fraction, index) {
+      if (clockSeconds < clockSecondsStartOfCraziness * fraction) {
+        singleLetterSpans[index].textContent = crazyCharacters[Math.floor(Math.random() * crazyCharacters.length)];
+      }
+    });
+
+    if (clockSeconds < clockSecondsStartOfCraziness - 10) {
+      $('.counter-reset').show();
+    }
+    if (clockSeconds < clockSecondsStartOfCraziness - 20) {
+      $('.counter-reset').addClass('counter-reset-big');
+    }
   }
 
   var interval = setInterval(tickClock, intervalMs);
@@ -49,15 +102,32 @@ $(function() {
   // Pause clock ticks when page is not active.
   addVisibilityHandler(function(visible) {
     clearInterval(interval);
-    if (visible) {
+    if (visible && enabled) {
       interval = setInterval(tickClock, intervalMs);
     }
   });
 
   $('.counter-numbers').click(function(event) {
     event.preventDefault();
-    intervalMs = 20;
+    if (enabled) {
+      decreaseMs = 20;
+    } else {
+      decreaseMs = 800;
+      clockSeconds = 60 * 10 - 1;
+      enabled = true;
+      clearInterval(interval);
+      interval = setInterval(tickClock, intervalMs);
+    }
+  });
+
+  $('.counter-reset').click(function(event) {
+    event.preventDefault();
+    enabled = false;
     clearInterval(interval);
-    interval = setInterval(tickClock, intervalMs);
+    originalTexts.forEach(function(text, index) {
+      singleLetterSpans[index].textContent = text;
+    });
+    $('.counter-reset').hide();
+    $('.counter-reset').removeClass('counter-reset-big');
   });
 });
